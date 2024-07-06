744d2a395575cebba8507a00bafed2d72b5a637a
Alex <51419598+alexrintt@users.noreply.github.com>
2024-07-06 15:21:14 -0300

    Update blog.config.ts

--- blog.config.ts
+++ blog.config.ts
@@ -1,9 +1,6 @@
 const socialMedia = `
 GitHub https://github.com/alexrintt/obsidian
-Telegram https://t.me/alexrintt
-Discord https://discord.alexrintt.io/
-Email mailto:reach@alexrintt.io
-Fork this blog https://alexrintt.io/obsidian/how-to-use-obsidian's-blog-template-(built-with-gatsby-and-github-actions-but-no-coding-skills-are-required)/
+Fork this blog https://alexrintt.github.io/obsidian/how-to-use-obsidian's-blog-template-(built-with-gatsby-and-github-actions-but-no-coding-skills-are-required)/
 `;
 
 export default {
@@ -18,7 +15,7 @@ export default {
     twitterUsername: `<your-tt-username>`,
     description: `Obsidian is a blog and template built with Gatsby and GitHub, free and open-source.`,
     image: `https://user-images.githubusercontent.com/51419598/240399860-a9fb362c-28a0-4838-b8d5-8332c2feecb5.png`,
-    siteUrl: `https://alexrintt.io`,
+    siteUrl: `https://alexrintt.github.io`,
   },
   socialMedia: socialMedia,
   postsPerPage: 10,
